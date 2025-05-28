fa5ef1ae64a61b6eac43155b8f901d106def14be
ChenLin Wang <wangchenlin2001@icloud.com>
2025-05-28 19:21:49 +0800

    V1.1.11.2 增加 Progress 枚举器

--- Sources/WhooshingClient/Basic/ProgressContext.swift
+++ Sources/WhooshingClient/Basic/ProgressContext.swift
@@ -93,3 +93,137 @@ public struct ProgressContext: Sendable, CustomStringConvertible {
         .init(index: index + 1, done: done, bytes: dataSize, curBytes: curBytes + dataSize, totalBytes: totalBytes, startDate: startDate)
     }
 }
+
+/// 一个支持顺序生成固定数量 `ProgressContext` 的序列，用于模拟或追踪迭代任务进度。
+///
+/// 可以使用 for-in loop 进行遍历操作:
+///
+/// 提供 数据块大小 以及 总数据大小 以分配 Progress。例如，以下表示该分配进度的总大小为
+///  8KB，且以 1KB 的小块为一步
+///
+/// ```swift
+/// for ctx in Progress(chunk: 1024, bytes: 8192) {
+///     print(ctx)             // 打印出该 progress 的详细信息
+///     print(ctx.index)       // 从 0...7
+///     print(ctx.curBytes)    // 依次打印 1024, 2048, 3072 ... 8192
+///     print(...)
+///     // 进行一些数据生成操作
+/// }
+/// ```
+///
+/// 也可 提供 数据块个数 以及 数据块大小 以分配 Progress。以下分配的进度表示该分配进度的
+/// 数据块(执行的步数)数量为 10 个，且每一步的数据大小为 512，因此总大小将为
+/// 512 Bytes * 10 Pieces = 5120 Bytes
+///
+/// ```swift
+/// for ctx in Progress(pieces: 10, chunk: 512) {
+///     print(ctx)             // 打印出该 progress 的详细信息
+///     print(ctx.index)       // 从 0...9
+///     print(ctx.curBytes)    // 依次打印 512, 1024, 1536 ... 5120
+///     print(...)
+///     // 进行一些数据生成操作
+/// }
+/// ```
+///
+/// 或者 提供 数据块个数 以及 总数据大小 以分配 Progress。以下分配的进度表示该分配进度的
+/// 数据块(执行的步数)数量为 4 个，数据的总大小为 2KB
+///
+/// ```swift
+/// for ctx in Progress(pieces: 4, bytes: 2048) {
+///     print(ctx)             // 打印出该 progress 的详细信息
+///     print(ctx.index)       // 从 0...3
+///     print(ctx.curBytes)    // 依次打印 512, 1024, 1536, 2048
+///     print(...)
+///     // 进行一些数据生成操作
+/// }
+/// ```
+///
+/// 无需担心如果总大小无法被步数或数据大小整除的情况。若总大小并非数据大小的整倍数，Progress 也会
+/// 正确处理每次进度的大小，会将余数分配最后一个 progress 中
+///
+/// ```swift
+/// for ctx in Progress(pieces: 5, bytes: 5512) {
+///     print(ctx)             // 打印出该 progress 的详细信息
+///     print(ctx.index)       // 从 0...4
+///     print(ctx.curBytes)    // 依次打印 1378, 2756, 4134,
+///     print(...)
+///     // 进行一些数据生成操作
+/// }
+/// ```
+///
+public struct Progress: Sequence {
+    public typealias Element = ProgressContext
+    
+    private let chunk: UInt
+    private let total: UInt
+    
+    /// 提供 数据块大小 以及 总数据大小 以分配 Progress
+    ///
+    /// - Parameters:
+    ///   - chunk: 数据块的大小，以字节为单位
+    ///   - bytes: 总数据的大小
+    public init(chunk: UInt, bytes: UInt) {
+        self.chunk = chunk
+        self.total = bytes
+    }
+    
+    /// 提供 数据块个数 以及 数据块大小 以分配 Progress
+    ///
+    /// - Parameters:
+    ///   - pieces: 数据块的个数
+    ///   - bytes: 数据块的大小，以字节为单位
+    public init(pieces: UInt, chunk: UInt) {
+        self.chunk = chunk
+        self.total = pieces * chunk
+    }
+    
+    /// 提供 数据块个数 以及 总数据大小 以分配 Progress
+    ///
+    /// - Parameters:
+    ///   - pieces: 数据块的个数
+    ///   - bytes: 总数据的大小
+    ///
+    /// 自动切割数据块，若总大小非个数的倍数，会自动切分分配，例如：总大小 5013，
+    /// 个数为 5，则每次的数据大小为 1253, 1253, 1253, 1253, 1
+    ///
+    /// - Warning: 若总大小非个数的倍数，则
+    /// 实际进行的数据块的个数可能 = 所指定的个数 - 1。例如，总大小为 600，
+    /// 数据块个数为 7，则数据块将会以每步 100 的大小进行，因此只会进行 6 步。另外，所
+    /// 输入的数据块个数不可等于 0。因此使用该函数有一定的风险
+    public init(pieces: UInt, bytes: UInt) {
+        guard pieces != 0 else { fatalError("输入非法") }
+        if bytes % pieces == 0 {
+            self.chunk = bytes / pieces
+        } else {
+            self.chunk = bytes / (pieces - 1)
+        }
+        self.total = bytes
+    }
+
+    public struct Iterator: IteratorProtocol {
+        private var temp: ProgressContext?
+        private let chunk: Int
+
+        init(chunk: UInt, total: UInt) {
+            self.chunk = Int(chunk)
+            guard total > 0 else { self.temp = nil; return }
+            self.temp = .init(index: 0, curBytes: self.chunk, totalBytes: Int(total))
+        }
+
+        public mutating func next() -> ProgressContext? {
+            guard let current = temp else { return nil }
+            if current.done {
+                temp = nil
+                return current
+            }
+            let remains = current.totalBytes! - current.curBytes
+            let c = chunk < remains ? chunk : remains
+            temp = current.next(c, done: (remains - c) == 0)
+            return current
+        }
+    }
+
+    public func makeIterator() -> Iterator {
+        Iterator(chunk: chunk, total: total)
+    }
+}
---
+++ Tests/toolbox-client-Tests/Progress-Tests.swift
@@ -0,0 +1,83 @@
+import Testing
+@testable import WhooshingClient
+
+struct ProgressTests {
+    @Test("Progress 指定次数和步长")
+    func testPiecesChunkProgress() throws {
+        let step: UInt = 5
+        let chunk: UInt = 1000
+        let totalSize = step * chunk
+        
+        var seen: [ProgressContext] = []
+
+        for ctx in Progress(pieces: step, chunk: chunk) {
+            print(ctx)
+            seen.append(ctx)
+        }
+
+        #expect(seen.count == step)
+        #expect(seen.last?.done == true)
+
+        for (i, ctx) in seen.enumerated() {
+            #expect(ctx.index == i)
+            #expect(ctx.curBytes == UInt(i + 1) * chunk)
+            #expect(ctx.bytes <= chunk)
+        }
+
+        #expect(seen.last?.curBytes == Int(totalSize))
+    }
+    
+    @Test("Progress 指定次数和总大小")
+    func testPiecesBytesProgress() throws {
+        let step: UInt = 7
+        let totalSize: UInt = 7012
+        let chunk = totalSize / (step - 1)
+        
+        var seen: [ProgressContext] = []
+
+        for ctx in Progress(pieces: step, bytes: totalSize) {
+            print(ctx)
+            seen.append(ctx)
+        }
+
+        #expect(seen.count == step)
+        #expect(seen.last?.done == true)
+
+        for (i, ctx) in seen.enumerated() {
+            #expect(ctx.index == i)
+            if i < (seen.count - 1) {
+                #expect(ctx.curBytes == UInt(i + 1) * chunk)
+            }
+            #expect(ctx.bytes <= chunk)
+        }
+
+        #expect(seen.last?.curBytes == Int(totalSize))
+    }
+    
+    @Test("Progress 指定步长和总大小")
+    func testChunkBytesProgress() throws {
+        let totalSize: UInt = 8888
+        let chunk: UInt = 1000
+        let step = totalSize / chunk + 1
+        
+        var seen: [ProgressContext] = []
+
+        for ctx in Progress(chunk: chunk, bytes: totalSize) {
+            print(ctx)
+            seen.append(ctx)
+        }
+
+        #expect(seen.count == step)
+        #expect(seen.last?.done == true)
+
+        for (i, ctx) in seen.enumerated() {
+            #expect(ctx.index == i)
+            if i < (seen.count - 1) {
+                #expect(ctx.curBytes == UInt(i + 1) * chunk)
+            }
+            #expect(ctx.bytes <= chunk)
+        }
+
+        #expect(seen.last?.curBytes == Int(totalSize))
+    }
+}
