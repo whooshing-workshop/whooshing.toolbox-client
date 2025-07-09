dc6ccefa3d17f9e8dfbc12695691016bbf2851dd
ChenLin Wang <wangchenlin2001@icloud.com>
2025-07-09 15:39:59 +0800

    V1.2.2.11 添加 Sendable 标志

--- Sources/WhooshingClient/Extensions/Basics.swift
+++ Sources/WhooshingClient/Extensions/Basics.swift
@@ -9,6 +9,12 @@ extension ChannelHandlerContext: @retroactive @unchecked Sendable {}
 extension HTTPRequestEncoder: @retroactive @unchecked Sendable {}
 extension NIOHTTPRequestHeadersValidator: @retroactive @unchecked Sendable {}
 
+extension Crypto.Symm.Key: @unchecked Sendable{}
+extension Crypto.Asym.CPrivateKey: @unchecked Sendable{}
+extension Crypto.Asym.CPublicKey: @unchecked Sendable{}
+extension Crypto.Asym.SPrivateKey: @unchecked Sendable{}
+extension Crypto.Asym.SPublicKey: @unchecked Sendable{}
+
 public extension URL {
     @inlinable
     func toUri(with path: String) -> WebURI { .init(stringLiteral: self.absoluteString + path) }
