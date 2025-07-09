f6ebb19fd0485ccfe9dab3193314d0ee3894f870
ChenLin-Wang <wangchenlin2001@icloud.com>
2025-07-09 15:50:19 +0800

    V1.2.2.12 增加 `@retroactive` 标记

--- Sources/WhooshingClient/Extensions/Basics.swift
+++ Sources/WhooshingClient/Extensions/Basics.swift
@@ -9,11 +9,11 @@ extension ChannelHandlerContext: @retroactive @unchecked Sendable {}
 extension HTTPRequestEncoder: @retroactive @unchecked Sendable {}
 extension NIOHTTPRequestHeadersValidator: @retroactive @unchecked Sendable {}
 
-extension Crypto.Symm.Key: @unchecked Sendable{}
-extension Crypto.Asym.CPrivateKey: @unchecked Sendable{}
-extension Crypto.Asym.CPublicKey: @unchecked Sendable{}
-extension Crypto.Asym.SPrivateKey: @unchecked Sendable{}
-extension Crypto.Asym.SPublicKey: @unchecked Sendable{}
+extension Crypto.Symm.Key: @retroactive @unchecked Sendable{}
+extension Crypto.Asym.CPrivateKey: @retroactive @unchecked Sendable{}
+extension Crypto.Asym.CPublicKey: @retroactive @unchecked Sendable{}
+extension Crypto.Asym.SPrivateKey: @retroactive @unchecked Sendable{}
+extension Crypto.Asym.SPublicKey: @retroactive @unchecked Sendable{}
 
 public extension URL {
     @inlinable
